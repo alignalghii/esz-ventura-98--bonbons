7068a80d07acd5d5deb9e940c41a83019ef1b71f
Endrey Márk <ungazik@gmail.com>
2020-08-24 21:53:19 +0200

    Console options: check winning strategy, or trace a complete game tree

--- app/Main.hs
+++ app/Main.hs
@@ -14,32 +14,42 @@ import Data.Monoid (mconcat)
 import Web.Scotty.Internal.Types (ActionT)
 import Data.Text.Internal.Lazy (Text)
 
-data Flag = Help | Web | Desktop | RowsNum String | ColsNum String
+import Data.Bool (bool)
+
+
+data Flag = Help | Server | ConsoleTrace | ConsoleCheck | RowsNum String | ColsNum String
 
 optDescriptions :: [OptDescr Flag]
 optDescriptions =
     [    Option "h?" ["help", "info"]  (NoArg Help)               "Help for usage"
-    ,    Option "wS" ["web", "server"] (NoArg Web)                "Web application mode (server)"
-    ,    Option "d"  ["desktop"]       (NoArg Desktop)            "Desktop application mode (simple command-line)"
-    ,    Option "mr" ["rowsnum"]       (ReqArg RowsNum "ROWSNUM") "ROWSNUM stands for a natural number (0, 1, 2, ...) for the number of rows"
-    ,    Option "nc" ["colsnum"]       (ReqArg ColsNum "COLSNUM") "COLSNUM stands for a natural number (0, 1, 2, ...) for the number of columns"
+    ,    Option "wS" ["web", "server"] (NoArg Server)             "Web application mode (server)"
+    ,    Option "t"  ["console-trace"] (NoArg ConsoleTrace)       "Console utility to list game tree as rawtext"
+    ,    Option "c"  ["console-check"] (NoArg ConsoleCheck)       "Console utility to check solubility (`Has Begginer Player a winning strategy or not?')"
+    ,    Option "m"  ["rowsnum"]       (ReqArg RowsNum "ROWSNUM") "ROWSNUM stands for a natural number (0, 1, 2, ...) for the number of rows"
+    ,    Option "n"  ["colsnum"]       (ReqArg ColsNum "COLSNUM") "COLSNUM stands for a natural number (0, 1, 2, ...) for the number of columns"
     ]
 
 main :: IO ()
 main = do
     args <- getArgs
     case getOpt Permute optDescriptions args of
-        ([Desktop, RowsNum m, ColsNum n], nonOpts, errorMsgs) -> desktopMain m n
-        ([Web]                          , nonOpts, errorMsgs) -> webMain
-        ([Help]                         , nonOpts, errorMsgs) -> putStrLn info
-        (_                              , nonOpts, errorMsgs) -> die      info
+        ([Server]                            , nonOpts, errorMsgs) -> webMain
+        ([ConsoleTrace, RowsNum m, ColsNum n], nonOpts, errorMsgs) -> traceMain m n
+        ([ConsoleCheck, RowsNum m, ColsNum n], nonOpts, errorMsgs) -> checkMain m n
+        ([Help]                              , nonOpts, errorMsgs) -> putStrLn info
+        (_                                   , nonOpts, errorMsgs) -> die      info
 
 info :: String
 info = usageInfo "Usage:\n\tRunning as dektop application:.....    bonbons --desktop -m ROWSNUM -n COLSNUM\n\tRunning as web application:........    bonbons --server\nOptions:" optDescriptions
 
-desktopMain :: String -> String -> IO ()
-desktopMain rowsNumString colsNumString = case map reads [rowsNumString, colsNumString] of
+traceMain :: String -> String -> IO ()
+traceMain rowsNumString colsNumString = case map reads [rowsNumString, colsNumString] of
     [(rowsNum, _) : _, (colsNum, _): _] -> if rowsNum >= 0 && rowsNum >= 0 then putStrLn $ showInd 0 (game (rowsNum, colsNum)) "" else failValidation
+    _                                -> failValidation
+
+checkMain :: String -> String -> IO ()
+checkMain rowsNumString colsNumString = case map reads [rowsNumString, colsNumString] of
+    [(rowsNum, _) : _, (colsNum, _): _] -> if rowsNum >= 0 && rowsNum >= 0 then putStrLn $ bool "Follower Player has winning strategy" "Beginner Player has winning strategy" $ startPlayerHasWiningStrategy $ game (rowsNum, colsNum) else failValidation
     _                                -> failValidation
 
 failValidation :: IO ()
